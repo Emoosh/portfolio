7b0dae3841faf908c7d23af9b6366afc36804302
Emoosh <161652763+Emoosh@users.noreply.github.com>
2024-09-19 00:36:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,60 @@
-window.addEventListener('wheel', function(event) {
-    event.preventDefault();
-    window.scrollBy({
-        top: event.deltaY * 7.5,  
-        behavior: 'smooth'  
+const sections = document.querySelectorAll('.section');
+let currentSectionIndex = 0;
+let isScrolling = false;
+
+function scrollToSection(index) {
+    console.log(`Scrolling to section ${index}`); // Test için eklenen satır
+    sections[index].scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest'
     });
+}
+
+window.addEventListener('wheel', function(event) {
+    event.preventDefault(); // Varsayılan kaydırmayı durdurur
+    if (isScrolling) return; 
+
+    isScrolling = true;
+
+    if (event.deltaY > 0) {
+        // Aşağı kaydırma
+        if (currentSectionIndex < sections.length - 1) {
+            currentSectionIndex++;
+        }
+    } else {
+        // Yukarı kaydırma
+        if (currentSectionIndex > 0) {
+            currentSectionIndex--;
+        }
+    }
+
+    scrollToSection(currentSectionIndex);
+
+    setTimeout(() => {
+        isScrolling = false; 
+    }, 1000);
 }, { passive: false });
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+            } else {
+                entry.target.classList.remove('active');
+            }
+        });
+    }, {
+        threshold: 0.1  
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
+
